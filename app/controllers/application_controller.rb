1afc835ab31b52bd84bc1e70eb1427fefc87d7bb
Dimetriu <dima.rashidov@gmail.com>
2019-05-23 09:14:33 +0300

    add cookies support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  # Enables cookie storage
+  include ::ActionController::Cookies
 end
--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,9 @@ module ShortencraftsRails
       g.factory_bot suffix: 'factory'
     end
 
+    # Add middleware to use cookies
+    config.middleware.use ActionDispatch::Cookies
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
